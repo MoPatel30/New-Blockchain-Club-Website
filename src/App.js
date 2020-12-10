3ef40bbf3391797403748a5d13f3f159ac966540
Mohammed Patel <mopatel1214@gmail.com>
2020-12-10 00:31:43 -0600

    temporary header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+//import Bootstrap from "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>New Club Website</h1>
+
+      <nav className = "navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+       
+        <h1 style = {{color: "white"}}><i>Blockchain@LUC</i></h1>
+       
+        <div class="container">
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                  <a class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link">Education</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link">About</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+      </nav>
+
     </div>
   );
 }
