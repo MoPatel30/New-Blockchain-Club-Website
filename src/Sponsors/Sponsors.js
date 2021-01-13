bddce1fe9a01739543107af40b2d8161841829f9
Kevin Guillaume <keving3492@gmail.com>
2021-01-12 22:15:33 -0600

    Fix slider to be continous

--- src/Sponsors/Sponsors.js
+++ src/Sponsors/Sponsors.js
@@ -7,19 +7,19 @@ import './Sponsors.css'
 
 function Sponsors(slides){
     const [current, setCurrent] = useState(0)
-    const length = slides.length
-    console.log(Object.keys(SponsorData).length)
-
+    const length = Object.keys(SponsorData).length
+    
+    
 
 
     const next = () => {
         setCurrent(current === length-1 ? 0 : current + 1);
     }
 
-    const prev = () =>{
-        setCurrent(current === 0 ? length - 1 : current - 1);
+    const prev = () => {
+        setCurrent(current === 0 ? length-1 : current - 1);
     }
-
+    
 
     return (
         <div className="sponsors">
