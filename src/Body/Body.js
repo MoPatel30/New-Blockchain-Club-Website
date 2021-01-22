76dbff48df7733fa55de060e2f704daa30df7684
Mohammed Patel <mopatel1214@gmail.com>
2021-01-21 21:56:02 -0600

    some styling, added logo

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import "./Body.css"
+import logo from "./logo.jpg"
 
 
 function Body() {
     return (
         <div className = "body-part">
-            <h2 id = "welcome-tag"><i>Welcome to Blockchain@LUC</i></h2>
-            <p>Our mission is to spread awareness of blockchain technology, its history, its continuing impact, and relevant projects. In maintaining this focus, we strive to educate members with a quality understanding of what this space offers, creating leaders who can use their knowledge to seamlessly adapt to the inevitable transition to a fully digitized world as the Information Age progresses.</p>
+            <div>
+                <h2 id = "welcome-tag" style = {{fontWeight: "900"}}><i>Welcome to Blockchain@LUC</i></h2>
+                <p style = {{fontWeight: "900"}}>Our mission is to spread awareness of blockchain technology, its history, its continuing impact, and relevant projects. In maintaining this focus, we strive to educate members with a quality understanding of what this space offers, creating leaders who can use their knowledge to seamlessly adapt to the inevitable transition to a fully digitized world as the Information Age progresses.</p>
+            </div>
+            <img src = {logo} alt = "club logo" style = {{width: "600px", height: "400px"}} />
         </div>
     )
 }
