cc1b5eb58ffec1a9ddbd01d9c080d5d817ebcdf9
Mohammed Patel <mopatel1214@gmail.com>
2021-01-24 21:56:22 -0600

    small styling change, contact styling bug fixed

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -4,20 +4,20 @@ import "./Contact.css"
 
 function Contact() {
     return ( 
-            <div className="contact-us">
-                <div className="title-contact">Contact Us</div>
-                <div className = "contact-form">
-                    <div className="inputs">
-                        <input type = "text" class= "input" placeholder= "Name"></input>
-                        <input type = "text" class= "input" placeholder = "Email"></input>
-                        <input type = "text" class= "input" placeholder = "Phone Number? Idk what else"></input>
-                    </div>
+        <div className="contact-us">
+            <div className = "contact-form">
+                <div className="inputs">
+                    <h1 style = {{color:  "white"}}><strong>Contact Us</strong></h1>
+                    <input type = "text" class= "input" placeholder= "Name"></input>
+                    <input type = "text" class= "input" placeholder = "Email"></input>
+                    <input type = "text" class= "input" placeholder = "Subject"></input>
+                </div>
                 <div className = "message">
                     <textarea placeholder="How can we help?"></textarea>
                     <div className = "submit-btn" type = "submit">Send</div>
                 </div>
-                </div>
             </div>
+        </div>
     )
 }
 
